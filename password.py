bb0a3ac976e9a805eedae14d626db30fd65c55b0
hcchang14 <zhang811210@gmail.com>
2020-11-11 22:37:39 +0800

    basic version

---
+++ password.py
@@ -0,0 +1,13 @@
+#password
+times = 3
+while times <= 3 and times > 0 :
+    real_password = 'a123456'
+    password = input('請輸入密碼: ')
+    if real_password == password:
+        print('登入成功!')
+        break
+    else:
+        times = times - 1
+        print('密碼錯誤還有',times, '次機會')
+
+    
